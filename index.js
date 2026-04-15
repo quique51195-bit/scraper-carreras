55d3a0d5f7dcb76c213d1961e6f60a166d3f1680
quique51195-bit <quique.51195@gmail.com>
2026-04-15 23:17:42 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
+
+app.get('/extraer', async (req, res) => {
+    const urlDestino = req.query.url;
+    if (!urlDestino) return res.status(400).send('Falta la URL');
+
+    try {
+        const browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox', 
+                '--disable-setuid-sandbox', 
+                '--disable-dev-shm-usage',
+                '--disable-gpu'
+            ]
+        });
+        const page = await browser.newPage();
+        
+        // Esperamos a que la web de la carrera cargue completamente sus datos
+        await page.goto(urlDestino, { waitUntil: 'networkidle2', timeout: 60000 });
+        const html = await page.content();
+        await browser.close();
+        
+        res.send(html);
+    } catch (error) {
+        res.status(500).send('Error extrayendo la web: ' + error.message);
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Scraper funcionando en puerto ${PORT}`));
